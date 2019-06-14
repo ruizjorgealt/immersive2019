575d8eba9373ba196b6a9c5f53e8d8a83035db4c
root <jorgearuiz@icloud.com>
2019-06-14 03:55:17 +0000

    update

--- terraform-example/main.tf
+++ terraform-example/main.tf
@@ -1,15 +1,14 @@
 provider "aws" {
-  # access_key = "foo"
-  # secret_key = "bar"
-  region = "us-east-2"
+  region     = "us-west-2"
 }
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0c55b159cbfafe1f0"
-  instance_type          = "t2.micro"
-  key_name               = "portfolio"
-  subnet_id              = "subnet-c7a18faf"
+  ami                         = "ami-id"
+  instance_type               = "t2.micro"
+  key_name                    = "terraform"
+  subnet_id                   = "subnet-id"
   associate_public_ip_address = "true"
+
   vpc_security_group_ids = ["${aws_security_group.allow_ssh_http.id}"]
 
   user_data = <<-EOF
@@ -18,39 +17,39 @@ resource "aws_instance" "web" {
   apt-get install python -y
   EOF
 
-  tags {
+  tags = {
     Name = "${var.ec2_instance_name}"
   }
 }
 
 variable "ec2_instance_name" {
-  default     = "My EC2 Instance"
-  description = "Name of EC2 instance"
-  type        = "string"
+  # default     = "My EC2 Instance"
+  description   = "Name of EC2 instance"
+  type          = "string"
 }
 
 resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow_ssh_http"
+  name = "allow_ssh_http"
   description = "Allows SSH and HTTP"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
--- terraform-example/terra-install.sh
+++ terraform-example/terra-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt-get update
-apt-get install unzip
+apt-get install unzip wget -y
 wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
 unzip terraform_0.12.2_linux_amd64.zip
 mv terraform /usr/local/bin/
