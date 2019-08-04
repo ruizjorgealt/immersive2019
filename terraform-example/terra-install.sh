0761129813ce837f05f2b93bc3e73ecea2f75526
Jorge A. Ruiz <39965509+ruizjorgealt@users.noreply.github.com>
2019-08-04 13:48:30 -0700

    Update terra-install.sh

--- terraform-example/terra-install.sh
+++ terraform-example/terra-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
+apt-get update -y
 apt-get install unzip wget -y
 wget https://releases.hashicorp.com/terraform/0.12.2/terraform_0.12.2_linux_amd64.zip
 unzip terraform_0.12.2_linux_amd64.zip
